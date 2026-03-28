eb271a432cc0a90b0d9c2c8f3e4e3319dee1490b
VincentP <vincentpote@hotmail.com>
2026-03-28 20:12:59 +0100

    Fix infinite loop when not logged in

--- frontend/src/routes/auth/login.tsx
+++ frontend/src/routes/auth/login.tsx
@@ -4,30 +4,33 @@ import { api } from '../../api.ts'
 import LoginForm from '../../components/loginForm'
 
 export const Route = createFileRoute('/auth/login')({
-    component: RouteComponent,    
-    beforeLoad: async () => {
-        // If someone bookmarked the login page, send them immediatly to the app if they are still logged in
-        try {
-          const res = await api.get("/auth/me");
-          const authStore = useAuthStore.getState();
-          authStore.setUser({
-            username: res.data.username,
-            role: res.data.role,
-          });      
-        } catch {
-          throw redirect({ to: "/auth/login" });
-        }
-        throw redirect({ to: "/app" });
-      },
+  component: RouteComponent,
+  beforeLoad: async () => {
+    // If someone bookmarked the login page, send them immediatly to the app if they are still logged in 
+    let shouldRedirectToApp = false;
+    try {
+      const res = await api.get("/auth/me");
+      const authStore = useAuthStore.getState();
+      authStore.setUser({
+        username: res.data.username,
+        role: res.data.role,
+      });
+      shouldRedirectToApp = true;
+    } catch {
+      console.log("Not logged in");
+    }
+    if (shouldRedirectToApp)
+      return redirect({ to: "/app" });
+  },
 })
 
 function RouteComponent() {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm">
-                <LoginForm />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
 
