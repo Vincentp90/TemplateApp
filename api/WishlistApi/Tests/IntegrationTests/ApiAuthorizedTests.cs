409f9868f51d07ff39c3486ff29f8c2dac6df242
VincentP <vincentpote@hotmail.com>
2026-05-06 12:29:53 +0200

    Split ApiTests into authorized and unauthorized

--- api/WishlistApi/Tests/IntegrationTests/ApiAuthorizedTests.cs
+++ api/WishlistApi/Tests/IntegrationTests/ApiAuthorizedTests.cs
@@ -1,92 +1,26 @@
 ﻿using DataAccess;
 using DataAccess.AppListings;
-using Docker.DotNet.Models;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http.Json;
-using System.Text;
 using System.Text.Json;
 using Tests.Helpers;
 using WishlistApi.DTOs;
 using FluentAssertions;
-using Renci.SshNet;
 
 namespace Tests.IntegrationTests
 {
-    // TODO reorganize/split in smaller files. Tests of authorized methods in separate class
-    public class ApiTests : IClassFixture<ApiFactory>
+    public class ApiAuthorizedTests : IClassFixture<ApiFactory>
     {
         private readonly HttpClient _client;
         private readonly ApiFactory _apiFactory;
 
-        public ApiTests(ApiFactory factory)
+        public ApiAuthorizedTests(ApiFactory factory)
         {
             _client = factory.CreateClient();
             _apiFactory = factory;
-        }
-
-        [Fact]
-        public async Task Post_register_returns_ok()
-        {
-            var request = new
-            {
-                Username = "testuser1",
-                Password = "Test123!"
-            };
-
-            var response = await _client.PostAsJsonAsync("/auth/register", request);
-
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
-
-        [Fact]
-        public async Task Register_WithExistingUsername_ReturnsBadRequest()
-        {
-            var request = new
-            {
-                Username = "duplicateUser",
-                Password = "Test123!"
-            };
-
-            // first call succeeds
-            var first = await _client.PostAsJsonAsync("/auth/register", request);
-            first.EnsureSuccessStatusCode();
-
-            // second call should fail
-            var second = await _client.PostAsJsonAsync("/auth/register", request);
-
-            Assert.Equal(HttpStatusCode.BadRequest, second.StatusCode);
-
-            var content = await second.Content.ReadAsStringAsync();
-            Assert.Contains("Username already taken", content);
-        }
-
-        [Fact]
-        public async Task LoginTest()
-        {
-            // Arrange
-            var userName = new Guid().ToString();
-            var password = new Guid().ToString();
-
-            var login = new { Username = userName, Password = password };
-            await _client.PostAsJsonAsync("/auth/register", login);
-
-            // Act
-            var response = await _client.PostAsJsonAsync("/auth/login", login);
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            Assert.True(response.Headers.TryGetValues("Set-Cookie", out var cookies));
-
-            var authCookie = cookies.FirstOrDefault(c => c.StartsWith("auth_token"));
-            Assert.NotNull(authCookie);
-
-            Assert.Contains("HttpOnly", authCookie, StringComparison.OrdinalIgnoreCase);
-            Assert.Contains("Secure", authCookie, StringComparison.OrdinalIgnoreCase);
         }
 
         [Fact]
@@ -113,7 +47,7 @@ namespace Tests.IntegrationTests
             string OLDESTAPPNAME = "Old";
 
             var secureClient = await CreateNewAnonymousUserAndGetSecureClient();
-            int appIdRandomOffset = Random.Shared.Next(0, Int32.MaxValue/4);
+            int appIdRandomOffset = Random.Shared.Next(0, Int32.MaxValue / 4);
             await _apiFactory.SeedAsync(async sp =>
             {
                 var dbContext = sp.GetRequiredService<WishlistDbContext>();
@@ -169,10 +103,10 @@ namespace Tests.IntegrationTests
 
             await _client.PostAsJsonAsync("/auth/register", login);
 
-            var response = await _client.PostAsJsonAsync("/auth/login", login);            
+            var response = await _client.PostAsJsonAsync("/auth/login", login);
             var authCookie = response.Headers.GetValues("Set-Cookie").First(c => c.StartsWith("auth_token"));
 
-            return new UserAndCookie { Username = userName, Password = password, Cookie = authCookie }; 
+            return new UserAndCookie { Username = userName, Password = password, Cookie = authCookie };
         }
 
         private async Task<HttpClient> GetSecureClient(UserAndCookie userAndCookie)
---
+++ api/WishlistApi/Tests/IntegrationTests/ApiUnauthorizedTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using Tests.Helpers;
+
+namespace Tests.IntegrationTests
+{
+    public class ApiUnauthorizedTests : IClassFixture<ApiFactory>
+    {
+        private readonly HttpClient _client;
+
+        public ApiUnauthorizedTests(ApiFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Post_register_returns_ok()
+        {
+            var request = new
+            {
+                Username = "testuser1",
+                Password = "Test123!"
+            };
+
+            var response = await _client.PostAsJsonAsync("/auth/register", request);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Register_WithExistingUsername_ReturnsBadRequest()
+        {
+            var request = new
+            {
+                Username = "duplicateUser",
+                Password = "Test123!"
+            };
+
+            // first call succeeds
+            var first = await _client.PostAsJsonAsync("/auth/register", request);
+            first.EnsureSuccessStatusCode();
+
+            // second call should fail
+            var second = await _client.PostAsJsonAsync("/auth/register", request);
+
+            Assert.Equal(HttpStatusCode.BadRequest, second.StatusCode);
+
+            var content = await second.Content.ReadAsStringAsync();
+            Assert.Contains("Username already taken", content);
+        }
+
+        [Fact]
+        public async Task LoginTest()
+        {
+            // Arrange
+            var userName = new Guid().ToString();
+            var password = new Guid().ToString();
+
+            var login = new { Username = userName, Password = password };
+            await _client.PostAsJsonAsync("/auth/register", login);
+
+            // Act
+            var response = await _client.PostAsJsonAsync("/auth/login", login);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.True(response.Headers.TryGetValues("Set-Cookie", out var cookies));
+
+            var authCookie = cookies.FirstOrDefault(c => c.StartsWith("auth_token"));
+            Assert.NotNull(authCookie);
+
+            Assert.Contains("HttpOnly", authCookie, StringComparison.OrdinalIgnoreCase);
+            Assert.Contains("Secure", authCookie, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
